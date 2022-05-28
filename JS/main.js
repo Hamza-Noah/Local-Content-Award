33718ff2de5913903c4c2c9efcb85f47a48d7922
Hamza-Noah <hamza.nouh97@gmail.com>
2022-05-28 19:28:23 +0200

    improved the journey section

--- JS/main.js
+++ JS/main.js
@@ -1,37 +1,38 @@
 // The End Of The Year Date To Countdown To
 // 1000 milliseconds = 1 Second
 
-let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
-// console.log(countDownDate);
 
-let counter = setInterval(() => {
-  // Get Date Now
-  let dateNow = new Date().getTime();
-
-  // Find The Date Difference Between Now And Countdown Date
-  let dateDiff = countDownDate - dateNow;
-
-  // Get Time Units
-  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
-  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
-
-  document.querySelectorِ(".days span").innerHTML = days;
-  document.querySelectorِ(".hours span").innerHTML = hours;
-  document.querySelectorِ(".minutes span").innerHTML = minutes;
-  document.querySelectorِ(".seconds span").innerHTML = seconds;
-
-  if (dateDiff < 0) {
-    clearInterval(counter);
-  }
-}, 1000);
 
 // Carousels
 
 $(document).ready(function () {
-  
+
+
+
+  let counter = function () {
+    let year = new Date().getFullYear()
+    let countDownDate = new Date(`Dec 31, ${year} 23:59:59`).getTime();
+    let dateNow = new Date().getTime();
+    let dateDiff = countDownDate - dateNow;
+    let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+    $(".days span").html(days);
+    $(".hours span").html(hours);
+    $(".minutes span").html(minutes);
+    $(".seconds span").html(seconds);
+    if (dateDiff < 0) {
+      clearInterval(counter);
+    }
+  }
+
+  counter();
+
+  setInterval(counter, 100)
+
+
 
   $("#landing-carousel").owlCarousel({
     nav: true,
@@ -39,7 +40,6 @@ $(document).ready(function () {
     rtl: true,
     dots: false,
     loop: true,
-    // smartSpeed :900,
     nav: true,
     navText: [
       "<img src='../assets/images/arrow-right.svg'>",
@@ -53,7 +53,7 @@ $(document).ready(function () {
     nav: true,
     items: 4,
     rtl: true,
-    dots: false,
+    dots: true,
     loop: true,
     // smartSpeed :900,
     nav: true,
@@ -65,3 +65,4 @@ $(document).ready(function () {
   });
 
 });
+
