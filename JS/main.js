6a8629ff14766d2a1589f36551e418b9161eec78
Hamza-Noah <hamza.nouh97@gmail.com>
2022-05-28 11:34:43 +0200

    added the improvments to the project

---
+++ JS/main.js
@@ -0,0 +1,67 @@
+// The End Of The Year Date To Countdown To
+// 1000 milliseconds = 1 Second
+
+let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
+// console.log(countDownDate);
+
+let counter = setInterval(() => {
+  // Get Date Now
+  let dateNow = new Date().getTime();
+
+  // Find The Date Difference Between Now And Countdown Date
+  let dateDiff = countDownDate - dateNow;
+
+  // Get Time Units
+  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelectorِAll(".days span").innerHTML = days;
+  document.querySelectorِAll(".hours span").innerHTML = hours;
+  document.querySelectorِAll(".minutes span").innerHTML = minutes;
+  document.querySelectorِAll(".seconds span").innerHTML = seconds;
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
+
+// Carousels
+
+$(document).ready(function () {
+  
+
+  $("#landing-carousel").owlCarousel({
+    nav: true,
+    items: 1,
+    rtl: true,
+    dots: false,
+    loop: true,
+    // smartSpeed :900,
+    nav: true,
+    navText: [
+      "<img src='../assets/images/arrow-right.svg'>",
+      "<img src='../assets/images/arrow-left.svg'>",
+    ],
+    autoplay: true,
+  });
+
+
+  $("#partners-carousel").owlCarousel({
+    nav: true,
+    items: 4,
+    rtl: true,
+    dots: false,
+    loop: true,
+    // smartSpeed :900,
+    nav: true,
+    navText: [
+      "<img src='../assets/images/arrow-right.svg'>",
+      "<img src='../assets/images/arrow-left.svg'>",
+    ],
+    autoplay: true,
+  });
+
+});
